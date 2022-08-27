7b0ccf421ec84ee8070bcd5c7dc88ecc52e8dade
QuentiumYT <quentin.lienhardt@gmail.com>
2022-08-27 21:36:34 +0200

    Initialize bot as a class + added cogwatch

--- RandoBot.py
+++ RandoBot.py
@@ -1,5 +1,6 @@
-import nextcord, os
+import nextcord, os, asyncio
 from nextcord.ext import commands
+from cogwatch import watch
 
 from cogs.utils import config, cogs
 
@@ -8,56 +9,61 @@ __version__ = "2.0.0"
 
 
 
-intents = nextcord.Intents.all()
+class RandoBot(commands.Bot):
+    def __init__(self):
+        intents = nextcord.Intents.all()
 
-bot = commands.Bot(
-    command_prefix="-",
-    description="Rando Bot",
-    owner_id=246943045105221633,
-    help_command=None,
-    case_insensitive=True,
-    max_messages=999999,
-    intents=intents,
-)
-
-@bot.event
-async def on_ready():
-    print("Logged in as %s#%s" % (bot.user.name, bot.user.discriminator))
-
-    await bot.change_presence(
-        status=nextcord.Status.online,
-        activity=nextcord.Activity(
-            name="Prépare une rando...",
-            type=nextcord.ActivityType.playing
+        super().__init__(
+            command_prefix="-",
+            description="Rando Bot",
+            owner_id=246943045105221633,
+            help_command=None,
+            case_insensitive=True,
+            max_messages=999999,
+            intents=intents,
         )
-    )
 
-@bot.event
-async def on_raw_reaction_add(ctx):
-    if not ctx.member.bot:
-        if any(ctx.emoji.name == x for x in ["Check", "Average"]):
-            role = nextcord.utils.get(ctx.member.guild.roles, name=config["role_next_rando"])
-            try:
-                await ctx.member.add_roles(role)
-            except nextcord.errors.Forbidden:
-                pass
+    @watch(path="cogs")
+    async def on_ready(self):
+        print("Logged in as %s#%s" % (self.user.name, self.user.discriminator))
 
-@bot.event
-async def on_raw_reaction_remove(ctx):
-    guild = nextcord.utils.get(bot.guilds, id=ctx.guild_id)
-    user = nextcord.utils.get(guild.members, id=ctx.user_id)
-    if not user.bot:
-        if any(ctx.emoji.name == x for x in ["Check", "Average", "Cross"]):
-            role = nextcord.utils.get(guild.roles, name=config["role_next_rando"])
-            try:
-                await user.remove_roles(role)
-            except nextcord.errors.Forbidden:
-                pass
+        await self.change_presence(
+            status=nextcord.Status.online,
+            activity=nextcord.Activity(
+                name="Prépare une rando...",
+                type=nextcord.ActivityType.playing
+            )
+        )
+
+    async def on_raw_reaction_add(self, ctx):
+        if not ctx.member.bot:
+            if any(ctx.emoji.name == x for x in ["Check", "Average"]):
+                role = nextcord.utils.get(ctx.member.guild.roles, name=config["role_next_rando"])
+                try:
+                    await ctx.member.add_roles(role)
+                except nextcord.errors.Forbidden:
+                    pass
+
+    async def on_raw_reaction_remove(self, ctx):
+        guild = nextcord.utils.get(self.guilds, id=ctx.guild_id)
+        user = nextcord.utils.get(guild.members, id=ctx.user_id)
+        if not user.bot:
+            if any(ctx.emoji.name == x for x in ["Check", "Average", "Cross"]):
+                role = nextcord.utils.get(guild.roles, name=config["role_next_rando"])
+                try:
+                    await user.remove_roles(role)
+                except nextcord.errors.Forbidden:
+                    pass
 
 
 
-if __name__ == "__main__":
+async def main():
+    bot = RandoBot()
+
     for cog in cogs:
         bot.load_extension("cogs." + cog)
 
-    bot.run(os.environ.get("TOKEN"))
+    await bot.start(os.environ.get("TOKEN"))
+
+if __name__ == "__main__":
+    asyncio.run(main())
